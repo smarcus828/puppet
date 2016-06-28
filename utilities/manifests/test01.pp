06f9600f7642cacf865206fe93543fc89fb3d1b1
root <root@master.example.com>
2016-06-28 01:41:31 -0400

    First commit

--- utilities/manifests/test01.pp
+++ utilities/manifests/test01.pp
@@ -27,18 +27,64 @@ class utilities::test01 {
 #       path	=> ['/usr/bin','/bin'],
 #}
 
-#package { 'dialog' :
-#	ensure 	=> present,
+#exec { 'orange IS found' :   ####  THIS is better - this works #####
+#       command => "touch /tmp/ORANGE_IS_THERE",
+#       user 	=> root,
+#       onlyif	=> ['ls /tmp/orange.txt'],
+#       path	=> ['/usr/bin','/bin'],
 #}
 
-exec { 'check for /tmp/orange.txt':
-	command => 'printf HEllo', 
-        path	=> ['/usr/bin','/bin'],
-	unless  => "test ! -f /tmp/orange.txt",
+
+##############################################################################################################
+################  Had to give up on this - trying to install dialog if /tmp/orange.txt exists - never worked ##########
+#exec { "check_presence" :
+#	command => '/bin/true',
+#	onlyif	=> '/usr/bin/test -f /tmp/orange.txt',
+#	#before => Package['dialog'],
+#}
+#
+#package { 'dialog' :
+#	require => Exec["check_presence"],
+#	ensure 	=> present,
+#}
+##############################################################################################################
+##############################################################################################################
+
+
+##############################################################################################################
+####################### execs in sequence WORKS ##############################################################
+##############################################################################################################
+$cmdpath = [ "/bin", "/usr/bin" ]
+
+exec { 'something 1' :
+	path 	=> $cmdpath,
+	command	=> "touch /tmp/file1",
+#	onlyif	=> '/usr/bin/test -f /tmp/orange.txt'  		# runs only if a file is present
+	onlyif  => '/bin/rpm -qa | grep dialog'			# runs only if a package is installed
+
+}~>
+exec { 'something 2' :						# runs only if something 1 works
+	path 	=> $cmdpath,
+	command	=> "touch /tmp/file2",
+	refreshonly	=> true,
+}~>
+exec { 'something 3' :						# runs only if something 2 works
+	path 	=> $cmdpath,
+	command	=> "touch /tmp/file3",
+	refreshonly	=> true,
+}
+##############################################################################################################
+
+
+
+
+
+
+
+
+
 }
 
 
 
 
-
-}
